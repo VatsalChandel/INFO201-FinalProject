f2ad6a68a9f520c0363fc735dc3deb1a4603e6fe
JGRIFF5UW <jgriff5@uw.edu>
2022-06-01 22:03:58 -0700

    Brand Names

--- server.R
+++ server.R
@@ -88,7 +88,29 @@ server <- function(input, output) {
     type_df[type_df$brand == "Crystal","brand" ] <- "Crystal Light"
     type_df[type_df$brand == "Guayaki","brand" ] <- "Guayaki Yerba Mate"
     type_df[type_df$brand == "Brew","brand" ] <- "Dr Kombucha"
-    type_df[type_df$brand == "Crystal","brand" ] <- "Crystal Light"
+    type_df[type_df$brand == "Master","brand" ] <- "Master Brew"
+    type_df[type_df$brand == "Turkey","brand" ] <- "Turkey Hill"
+    type_df[type_df$brand == "Sparkling","brand" ] <- "Sparkling Ice"
+    type_df[type_df$brand == "Polar","brand" ] <- "Polar Frost"
+    type_df[type_df$brand == "Poland","brand" ] <- "Poland Spring"
+    type_df[type_df$brand == "Dr","brand" ] <- "Dr Pepper"
+    type_df[type_df$brand == "Mountain","brand" ] <- "Mountain Dew"
+    type_df[type_df$brand == "Soda","brand" ] <- "Soda Stream"
+    type_df[type_df$brand == "Premium","brand" ] <- "Premium Cola"
+    type_df[type_df$brand == "Fritz","brand" ] <- "Fritz Kola"
+    type_df[type_df$brand == "Afri","brand" ] <- "Afri Cola"
+    type_df[type_df$brand == "doc","brand" ] <- "Doc"
+    type_df[type_df$brand == "Sun","brand" ] <- "Sun Drop"
+    type_df[type_df$brand == "5","brand" ] <- "5 Hour Energy"
+    type_df[type_df$brand == "Proper","brand" ] <- "Proper Wild"
+    type_df[type_df$brand == "Vital","brand" ] <- "Vital 4U"
+    type_df[type_df$brand == "Zombie","brand" ] <- "Zombie Blood"
+    type_df[type_df$brand == "Best","brand" ] <- "Best Choice"
+    type_df[type_df$brand == "Alani","brand" ] <- "Alani Nu"
+    type_df[type_df$brand == "Coffee","brand" ] <- "Coffee Bean & Tea"
+    type_df[type_df$brand == "7","brand" ] <- "7 Eleven"
+    type_df[type_df$brand == "Tim","brand" ] <- "Tim Hortons"
+    type_df[type_df$drink == "doc Soda","drink" ] <- "Doc Soda"
     
    filtered_type_df <- type_df %>%
      filter(type %in% input$type_select)
@@ -96,13 +118,19 @@ server <- function(input, output) {
    blank_theme <- theme_bw() +
      theme(
        plot.background = element_blank(), # remove gray background
-       plot.title = element_text(hjust = 0.5)
+       plot.title = element_text(hjust = 0.5),
+       axis.text = element_text(angle = 90)
      )
     
      type_plot <- ggplot(data = filtered_type_df) +
       geom_col(mapping = aes(
         x = brand,
         y = caffeine_per_calories,
+        text = paste(
+          "Brand:", brand,
+          "\nCaffeine / Calories", caffeine_per_calories,
+          "\nDrink:", drink
+        ),
         fill = drink,
       )) +
        labs(
